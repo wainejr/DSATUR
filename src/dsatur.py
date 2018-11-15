869ae02c7a404b058a74d7930716b1081f621b9c
Waine Junior <waine2000@gmail.com>
2018-11-15 21:22:01 -0200

    Updated source files

--- src/dsatur.py
+++ src/dsatur.py
@@ -1,1 +1,47 @@
 #   DSATUR implementation
+
+import rw_csv
+
+
+def alg_dsatur(graph):
+	# graph format: [n][0] value of the node
+	# 							[n][n] node's neighbours
+	degrees = list()
+	saturation_degrees = [0] * len(graph)
+	coloring = [0] * len(graph)
+	uncolored_nodes = set(range(len(graph)))
+	index_maximun_degree = 0
+	maximum_degree = 0
+	color_counter = 1
+
+	# fill nodes degrees 
+	for node in graph:
+		degrees.append(len(node)-1)
+	# finds node with maximum degree	
+	for index in range(len(degrees)):
+		if(degrees[index] > maximum_degree):
+			index_maximum_degree = index
+			maximum_degree = degrees[index]
+	# updates saturation
+	for index in range(1, degrees[index_maximum_degree]+1):
+		saturation_degrees[graph[index_maximum_degree][index]-1] += 1
+	# coloring first node
+	coloring[index_maximum_degree] = color_counter
+	uncolored_nodes.remove(index_maximum_degree)
+	
+		
+	print(graph)
+	print(degrees)
+	print(index_maximum_degree)
+	print(saturation_degrees)
+	print(coloring)
+
+def main():
+	graph = rw_csv.read_data()
+	alg_dsatur(graph)
+	#print(graph)
+
+
+
+if __name__ == "__main__":
+	main()
--- src/rw_csv.py
+++ src/rw_csv.py
@@ -1,2 +1,23 @@
 #   file for reading the .csv file 
 #   that represents the graph
+
+import csv
+
+PATH_FILE = "data/grafo06.csv"
+
+def read_data():	
+	data = []	
+	with open(PATH_FILE) as f:
+		f = csv.reader(f, delimiter=',')
+		for row in f:
+			# print(row)
+			data.append(list(map(int, row)))
+	# print(data)
+	return data
+'''
+	f = open(PATH_FILE, "r")
+	content = f.read()
+	print("Aqui")
+	print (content)
+	f.close()
+'''
